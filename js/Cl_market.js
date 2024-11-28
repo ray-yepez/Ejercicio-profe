2c74a2761fa28d924e3d3c181817b2ded6430baa
ray-yepez <1001.30255498.ucla@gmail.com>
2024-11-27 21:00:46 -0400

    (feature): salida detallada y estilo

--- js/Cl_market.js
+++ js/Cl_market.js
@@ -9,11 +9,14 @@ export default class Cl_market {
 
     aPagarDetal(tipoEsp, porcIncremento) {
         let total = 0;
-        // let detalles = '';
+        let detalles = document.getElementById("detal");
        this.articulos.forEach(articulo => {
             const precio = articulo.precio(tipoEsp, porcIncremento);
-            console.log(precio);
-            // detalles = `${articulo.codigo}, ${articulo.tipo}, ${articulo.nombre}, ${articulo.costo} -> precio ${precio.toFixed(2)}`; 
+            detalles.innerHTML += `<p>${articulo.codigo}, ${articulo.tipo},
+             ${articulo.nombre}, ${articulo.costo} -> precio ${precio.toFixed(2)}</p>`;
+             //Tranquila que no va a salir con ese salto de linea, sino que van a estar
+             //todo pegado y cada cosa en una línea aparte. 
+             //Lo divido así por legibilidad
             total += precio;
         });
 return total;
@@ -21,11 +24,12 @@ return total;
 
     aPagarMayor(cantCadaArt) {
         let total = 0;
-        // let detalles = '';
+        let detalles = document.getElementById("mayor");
         this.articulos.forEach(articulo => {
             const precio = articulo.precioCantidad(cantCadaArt);
             console.log(precio);
-            // detalles ( `${articulo.codigo}, ${articulo.tipo}, ${articulo.nombre}, ${articulo.costo} -> precio ${precio.toFixed(2)}`);
+            detalles.innerHTML +=  `<p>${articulo.codigo}, ${articulo.tipo}, 
+            ${articulo.nombre}, ${articulo.costo} -> precio ${precio.toFixed(2)}</p>`;
             total += precio;
         });
         return total;
